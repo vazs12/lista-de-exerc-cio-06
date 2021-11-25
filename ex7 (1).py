35b9f93f7a1b63efec3f60a419d7df28c595220f
yasmin sampaio <89849826+vazs12@users.noreply.github.com>
2021-11-25 19:10:10 -0300

    Add files via upload

---
+++ ex7 (1).py
@@ -0,0 +1,6 @@
+lista = [4, 8, 15, 16, 23, 42]
+for i in range (0,6):
+    print(lista[i])
+print("Multiplicando por dois")
+for listas in lista: 
+    print(listas * 2)
---
+++ ex8.py
@@ -0,0 +1,18 @@
+media = int(input(" Digite a sua média de 0 a 10: "))
+frequencia = int(input(" Digite a sua frequência de 0 a 100: "))
+resultado = frequencia * 75/100
+
+if resultado < resultado: 
+   print("Você foi reprovado(a)")
+if resultado > resultado: 
+   print("Você está de recuperação")
+if media < 7: 
+   print("Você está de recuperação")
+if resultado > resultado: 
+   print("Você foi aprovado(a)")
+if media >= 7: 
+   print("Você foi aprovado(a)")
+
+    
+
+
