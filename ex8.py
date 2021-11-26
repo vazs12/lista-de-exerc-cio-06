fe3e280d5fd4cc4f9c6fc9095991b610f8bb8d45
yasmin sampaio <89849826+vazs12@users.noreply.github.com>
2021-11-26 10:33:07 -0300

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,11 @@
+media = int(input(" Digite a sua média de 0 a 10: "))
+frequencia = int(input(" Digite a sua frequência de 0 a 100: "))
+resultado = frequencia * 75/100
+
+if resultado < resultado: 
+   print("Você foi reprovado(a)")
+if resultado > resultado and media < 7 : 
+   print("Você está de recuperação")
+if resultado > resultado and media >= 7: 
+   print("Você foi aprovado(a)")
+   
